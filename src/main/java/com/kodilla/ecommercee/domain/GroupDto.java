8a46e7154cfd78de7ee83c72bf82d888cd635a7f
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-11-19 22:33:40 +0100

    JDP221202-2 Wstepna implementacja GroupController + lombok

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,23 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupDto {
 
     private Long id;
     private String name;
 
-    public GroupDto() {
-    }
-
-    public GroupDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
