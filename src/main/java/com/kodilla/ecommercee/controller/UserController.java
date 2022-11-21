5729634b4b971a4a7ecfe261fcab026cdd1e298a
Konrad <skalskikonrad@gmail.com>
2022-11-21 02:06:00 +0100

    JDP221202-5 wstepna implementacja

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,24 +1,18 @@
 package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.UserDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/shop/users")
-@RequiredArgsConstructor
-@Service
 public class UserController {
     @PostMapping
     public void createUser(@RequestBody UserDto userDto) {
     }
     @PutMapping(value = "statusUser/{userId}")
-    public UserDto banUser(@RequestBody long userId) {
+    public UserDto changeUserStatus(@RequestBody long userId) {
         return new UserDto();
     }
     @PutMapping(value = "{userId}")
-    public void changeUserStatus(@PathVariable long userId){
+    public void generateUserKey(@PathVariable long userId){
         }
     }
