39958a1a672f898c1689a88bcefe4bb44f7ea224
Konrad <skalskikonrad@gmail.com>
2022-12-06 22:19:34 +0100

    JDP221202-11 - encja Order - implementacja

---
+++ src/main/java/com/kodilla/ecommercee/entity/Order.java
@@ -0,0 +1,44 @@
+package com.kodilla.ecommercee.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ORDER_ID", unique = true)
+    private Long id;
+
+    @NotNull
+    @Column(name = "ORDER_DATE")
+    private LocalDate orderDate;
+
+    @NotNull
+    @Column(name = "IS_CONFIRMED")
+    private Boolean isConfirmed;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToMany(cascade = { CascadeType.PERSIST })
+    @JoinTable(
+            name = "JOIN_ORDERS_WITH_PRODUCTS",
+            joinColumns = { @JoinColumn(name = "ORDER_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "PRODUCT_ID") }
+    )
+    private List<Product> products = new ArrayList<>();
+}
