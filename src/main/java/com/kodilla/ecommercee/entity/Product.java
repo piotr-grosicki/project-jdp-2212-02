4aa1e19ccfc620bc154fdd1376072cf7d765bce5
oskar <oskarlietzau1@gmail.com>
2022-12-07 18:52:55 +0100

    JDP221202-12

--- src/main/java/com/kodilla/ecommercee/entity/Product.java
+++ src/main/java/com/kodilla/ecommercee/entity/Product.java
@@ -35,26 +35,3 @@ public class Product {
     private List<Cart> carts;
 }
 
-=======
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Entity
-@Table(name = "products")
-public class Product {
-    @Id
-    @GeneratedValue
-    @Column(name = "product_id", unique = true)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "GROUP_ID")
-    private Group group;
-}
