502fcb2432860cd14813ac0d2bd3b7a9229aaaa2
oskar <oskarlietzau1@gmail.com>
2022-12-07 18:52:55 +0100

    JDP221202-12

---
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDt.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ProductDt {
+
+    private long id;
+    private String name;
+    private long id_group;
+    private double price;
+
+}
