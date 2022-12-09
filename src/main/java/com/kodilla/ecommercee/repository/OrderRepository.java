40f1a6c94ef1a66a0f37058569e0d9ac8efbe5c6
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-12-09 21:54:52 +0100

    Repository / Poprawka na JpaRepository

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -1,12 +1,12 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.entity.Order;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Override
     List<Order> findAll();
