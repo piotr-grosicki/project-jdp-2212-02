59cfe7089cf3d66ef9ccc864f0b6c022c4a730d0
oskar <oskarlietzau1@gmail.com>
2022-12-12 19:12:21 +0100

    user repo fix

--- src/main/java/com/kodilla/ecommercee/service/UserService.java
+++ src/main/java/com/kodilla/ecommercee/service/UserService.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private final UserRepository uartRepository;
+    private final UserRepository userRepository;
 
     //private final User user;
 }
